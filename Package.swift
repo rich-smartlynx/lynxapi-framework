3cee82c87674d36a82688149a920f7b5c0aadbfc
rich-smartlynx <rich@smartlinks.mobi>
2026-02-11 07:27:00 -0500

    Update lynxapi URL to use main branch

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "lynxapi",
-            url: "https://github.com/rich-smartlynx/swift-framework/raw/1.0.1-dev/lynxapi.xcframework.zip",
+            url: "https://github.com/rich-smartlynx/swift-framework/raw/main/lynxapi.xcframework.zip",
             checksum: "020eb53790e2ab5418e518ee4eccf27f4f2be56bfb192e2a2dd846884b494058"
         ),
     ]
