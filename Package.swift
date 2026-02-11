388fc3af55c3b5200cd871516b60af755e317830
rich-smartlynx <rich@smartlinks.mobi>
2026-02-11 08:38:44 -0500

    Update lynxapi binary target URL to not use github

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "lynxapi",
-            url: "https://github.com/rich-smartlynx/swift-framework/raw/main/lynxapi.xcframework.zip",
+            url: "https://smartlinks.mobi/static/lynxapi.xcframework.zip",
             checksum: "020eb53790e2ab5418e518ee4eccf27f4f2be56bfb192e2a2dd846884b494058"
         ),
     ]
