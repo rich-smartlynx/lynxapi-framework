4d10b3be211a82213d27e1b10ffa1616d0a745a6
Rich Cann <rich@smartlinks.mobi>
2026-03-16 14:32:14 -0400

    Path must match the tag name not the branch name (wwho knew?)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "lynxapi",
-            url: "https://github.com/rich-smartlynx/lynxapi/releases/download/dev-1.0.6/lynxapi.xcframework.zip",
+            url: "https://github.com/rich-smartlynx/lynxapi/releases/download/dev-1.0.6-5/lynxapi.xcframework.zip",
             checksum: "c16dd72ce18a7ed0815fbd53d6550c30b063a6f9db9a50be53ee6b3ee9bbf529"
         ),
     ]
