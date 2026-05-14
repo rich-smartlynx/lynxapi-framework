290538c3c88f0e62a94bd4881b205b52846899c8
Rich Cann <rich@smartlinks.mobi>
2026-05-14 07:40:09 -0400

    Switched to modern Swift logging

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "lynxapi",
-            url: "https://github.com/rich-smartlynx/lynxapi-framework/releases/download/1.0.7/lynxapi.xcframework.zip",
-            checksum: "d1e49dec0453613dff05e732d2b8f8e982c6292aa84f09e0686d674c484ee917"
+            url: "https://github.com/rich-smartlynx/lynxapi-framework/releases/download/1.0.8/lynxapi.xcframework.zip",
+            checksum: "51f4635fffce80b3e583ae9c6dc40cfc14d0a90ec452006636f42c7e68d92f54"
         ),
     ]
 )
