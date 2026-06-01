63cd07f980639916ae0a33bf0a8164c17e935100
Rich Cann <rich@smartlinks.mobi>
2026-06-01 09:36:06 -0400

    better logging - fixed screen size calculation

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "lynxapi",
-            url: "https://github.com/rich-smartlynx/lynxapi-framework/releases/download/1.0.8/lynxapi.xcframework.zip",
-            checksum: "51f4635fffce80b3e583ae9c6dc40cfc14d0a90ec452006636f42c7e68d92f54"
+            url: "https://github.com/rich-smartlynx/lynxapi-framework/releases/download/1.0.9/lynxapi.xcframework.zip",
+            checksum: "998280fae425f3a059dfac4dfd9596ba0357644b3602c775186bca9a45d55930"
         ),
     ]
 )
